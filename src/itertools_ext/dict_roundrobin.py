21bb1de8d82e6ffafb940733d2973f3b93705453
Grant Williams <grant@grantwilliams.dev>
2022-05-23 16:03:45 -0500

    Merge Hash Utils (#2)
    * code to stream parquet files form s3
    
    * rename files to be README files
    
    * added hash utils, updated the parquet utils, and added an interesting itertools extension
    
    * update printing
    
    * fix hash_combine description
    
    Co-authored-by: GrantWilliams <grant.williams@digintent.com>

---
+++ src/itertools_ext/dict_roundrobin.py
@@ -0,0 +1,105 @@
+from collections import deque
+from itertools import cycle, islice, repeat
+from typing import Any, Dict, Generator, List, Tuple, Union
+
+Pair = Tuple[Any, Any]
+Record = Dict[Any, Any]
+
+def dict_roundrobin(d: Dict[Any, List[Any]], orient: str) -> Union[Generator[Pair, None, None], Generator[Record, None, None]]:
+    """
+    Return an iterable yielding column value pairs from a dictionary of lists. The iterable can
+    return pairs as a generator of (key, value) tuples, or a generator of dicts with only
+    non-exhausted key-value pairs.
+
+    parameters:
+        d (dict[Any, list[Any]]): dictionary of {key: [value]}
+        orient str: "pairs" or "records"
+            `"pairs"` returns a generator of (key, value) tuples (`Record`s)
+            `"records"` returns a generator of sub dict records with non-exhausted key-value pairs (`Record`s)
+
+    Examples:
+    >>> data = {
+            'Col_1': ['1a', '1b', '1c'],
+            'Col_2': ['2a', '2b'],
+            'Col_3': [3.0]
+        }
+    >>> list(dict_roundrobin(data, orient="records"))
+    >>> [
+            {
+                'Col_1': '1a',
+                'Col_2': '2a',
+                'Col_3': 3.0
+            },
+            {
+                'Col_1': '1a',
+                'Col_2': '2a'
+            },
+            {
+                'Col_1': 1
+            }
+        ]
+
+    >>> list(dict_roundrobin(data, orient="pairs"))
+        [
+            ('Col_1', 1),
+            ('Col_2', 2),
+            ('Col_3', 3.0),
+            ('Col_1', 1),
+            ('Col_2', 2),
+            ('Col_1', 1)
+        ]
+    """
+    if not isinstance(d, dict):
+        raise ValueError(f"dict_roundrobin(): `d` must be a dictionary of lists. `d` was of non-dictionary type: {type(d)}.")
+    
+    if not all(isinstance(v, list) for v in d.values()):
+        bad_kvs = "\n".join(f"{k}: {type(v)}" for k, v in d.items() if not isinstance(v, list))
+        raise ValueError(f"dict_roundrobin(): `d` must be a dictionary of lists. Non list records found:\n{bad_kvs}.")
+
+    num_active = len(d.keys())
+    nexts = cycle(iter(it).__next__ for it in [zip(repeat(k), v) for k, v in d.items()])
+
+    if orient == "pairs":
+        d = deque(maxlen=0)
+        pairs = True
+    elif orient == "records":
+        d = deque(maxlen=num_active)
+        pairs = False
+    else:
+        raise ValueError(f"orient must be `'records'` or `'pairs'` '{orient}' is not valid.")
+
+    while num_active:
+        try:
+            for next in nexts:
+                if pairs:
+                    yield next()
+                else:
+                    if len(d) == num_active:
+                        yield dict(list(d))
+                        d.clear()
+                    d.append(next())
+        except StopIteration:
+            # Remove the iterator we just exhausted from the cycle.
+            num_active -= 1
+            nexts = cycle(islice(nexts, num_active))
+
+
+def main() -> None:
+    import json # only used for pretty printing when running locally
+
+    data = {
+        'Col_1': ['1a', '1b', '1c'],
+        'Col_2': ['2a', '2b'],
+        'Col_3': [3.0],
+    }
+    
+    print("dict_roundrobin (pairs):\n-----------------")
+    print(json.dumps(list(dict_roundrobin(data, orient="pairs")), indent=2))
+
+    print()
+
+    print("dict_roundrobin (records):\n-----------------")
+    print(json.dumps(list(dict_roundrobin(data, orient="records")), indent=2))
+
+if __name__ == "__main__":
+    main()
---
+++ src/math/hash_utils.py
@@ -0,0 +1,47 @@
+from typing import Any, Callable, Generator, TypeVar
+
+T = TypeVar("T")
+
+def hash_combine32(h1: T, h2: T) -> T:
+    """ Implementation of std::boost's hash_combine function for 32-bit hash values
+    returns a mixed/combined result of the 2 (ideally) 32 bit hashed values
+    """
+    return h1 ^ (h2 + 0x9e3779b9 + (h1 << 6) + (h1 >> 2))
+
+def hash_extender(hasher1: Callable[[Any], T],
+                  hasher2: Callable[[Any], T],
+                  val: Any) -> Generator[T, None, None]:
+    """ Utility function used to approximate many hash functions from 2 strong hash
+    functions. Implementation based on the paper:
+    'Less Hashing, Same Performance: Building a Better Bloom Filter by Kirsch et al.'
+
+    The paper describes a technique that only requires two independent hash functions to
+    effectively implement heuristic data stuctures like bloom filters or Sketches.
+
+    This implementation produces infinite `good` hashes by returning a generator expression.
+    
+    The `hasher1` and `hasher2` should both be strong non cryptographic hashing functions e.g. murmurhash3()
+    
+    To get exactly `n` hashes an easy approach would be to use:
+    >>> `hashes = islice(hash_extender(hasher1, hasher2, val), n)`
+    """
+    h1 = hasher1(val)
+    h2 = hasher2(val)
+    yield from hash_value_extender(h1, h2, val)
+
+def hash_value_extender(h1: T, h2: T, val: Any) -> Generator[T, None, None]:
+    """ Same function as `hash_extender()`, except `hash_value_extender() expects 
+    `h1` and `h2` to be values from the hash functions instead of a Callable expression.
+
+    To get exactly `n` hashes an easy approach would be to use:
+    >>> `hashes = islice(hash_value_extender(h1, h2, val), n)`
+    """
+
+    yield h1
+    yield h2
+
+    i = 0
+    while True:
+        yield h1 + ((i+1)*h2)
+        i += 1
+
